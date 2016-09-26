cb7db7e99f732bb1eec6231c6abf713091b3d8cc
casillas081 <christ.schepmans@hotmail.com>
2016-09-26 09:58:22 +0200

    Debut Labo2

---
+++ Labo2Environ_Dev_Logiciels/Labo2Environ_Dev_Logiciels/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo2Environ_Dev_Logiciels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
